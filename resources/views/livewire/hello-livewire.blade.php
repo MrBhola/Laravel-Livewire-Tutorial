0fdd224fb44a9555faa84eb25b5f2cbc2dbc506e
= <bkhawas42@gmail.com>
2020-07-19 16:30:20 +0545

    laravel livewire episod 1

---
+++ resources/views/livewire/hello-livewire.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1 style="text-align: center; margin:10rem">Welcome to Laravel Live Wire Series !!!</h1>
+    <p style="text-align: center; margin:10rem">this is from live wire component</p>
+</div>
