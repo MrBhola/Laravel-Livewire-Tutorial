a2e22f22645038b43f991c1ab07a864f11105b79
= <bkhawas42@gmail.com>
2020-07-22 12:29:56 +0545

    Episode 2

--- app/Http/Livewire/HelloLivewire.php
+++ app/Http/Livewire/HelloLivewire.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class HelloLivewire extends Component
 {
+    public $name, $age, $test = [], $status = false;
     public function render()
     {
         return view('livewire.hello-livewire');
--- resources/views/livewire/hello-livewire.blade.php
+++ resources/views/livewire/hello-livewire.blade.php
@@ -1,4 +1,27 @@
-<div>
-    <h1 style="text-align: center; margin:10rem">Welcome to Laravel Live Wire Series !!!</h1>
-    <p style="text-align: center; margin:10rem">this is from live wire component</p>
+<div style="margin-left: 10rem">
+    <h1>Welcome to Laravel Live Wire Series !!!</h1>
+    <h1>Name :{{$name}}</h1>
+    <input type="text" wire:model="name">
+    <h1>Age : {{$age}}</h1>
+    <input type="number" wire:model="age">
+    <h1>Test : {{implode(', ',$test)}}</h1>
+    <select wire:model="test" multiple>
+        <option>Blood</option>
+        <option>Urine</option>
+        <option>Stool</option>
+    </select>
+    <h1>Status : @if($status) Completed @else Testing @endif</h1>
+    <input type="checkbox" wire:model="status">
 </div>
+<style>
+    input {
+        height: 30px;
+        width: 300px;
+        font-size: 24px
+    }
+
+    select {
+        font-size: 22px
+    }
+
+</style>
